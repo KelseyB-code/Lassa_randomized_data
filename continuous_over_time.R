2a31571fb0f544162de2424787ddfc94ca6f300d
Kelsey Butler <kelsey@Kelseys-MacBook-Air.local>
2021-05-27 11:29:42 -0400

    Added code to explore missing data

--- continuous_over_time.R
+++ continuous_over_time.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 library(dplyr)
 
-var = "BUN"
-search_term = "urea" #variable name in blood_chemistry_list
-filename = "Data/commcare-cleaned-data-2018-2020.csv"
+var = "AST"
+search_term = "aspartate" #variable name in blood_chemistry_list
+filename = "commcare-cleaned-data-2018-2020.csv"
 lassa <- read.csv(filename)
 
 lassa <- lassa %>%
@@ -44,45 +44,52 @@ long_data_raw <- long_data
 long_data <- distinct(long_data) 
 long_data <- long_data[complete.cases(long_data), ] 
 
-long_data <-long_data %>% 
-  group_by(ID) %>% 
-  filter(n()>=3)
+#long_data <-long_data %>% 
+  #group_by(ID) 
 
-write.csv(long_data, paste("Data/", var, "_over_time.csv", sep=""), row.names = F)
+#write.csv(long_data, paste("Data/", var, "_over_time.csv", sep=""), row.names = F)
 
 ####################################################################################
 # Graph BUN to cre ratio
 
-cre <- read.csv("Data/Cre_over_time.csv")
-bun <- read.csv("Data/BUN_over_time.csv")
-
-cre_bun <- merge(cre, bun,by=c("ID","day", "age", "outcome"))
-
-colnames(cre_bun)[5] <- "cre"
-colnames(cre_bun)[6] <- "bun"
-
-cre_bun$val <- cre_bun$bun/cre_bun$cre
-
-write.csv(cre_bun, paste("Data/BUN_CRE_ratio_over_time.csv", sep=""), row.names = F)
+# cre <- read.csv("Cre_over_time.csv")
+# bun <- read.csv("BUN_over_time.csv")
+# 
+# cre_bun <- merge(cre, bun,by=c("ID","day", "age", "outcome"))
+# 
+# colnames(cre_bun)[5] <- "cre"
+# colnames(cre_bun)[6] <- "bun"
+# 
+# cre_bun$val <- cre_bun$bun/cre_bun$cre
+# head(cre_bun)
+# long_data <- cre_bun
+#write.csv(cre_bun, paste("Data/BUN_CRE_ratio_over_time.csv", sep=""), row.names = F)
 
 ###################################################################################
 long_data$val = as.numeric(long_data$val)
+long_data$outcome <- as.factor(long_data$outcome)
 
 #pdf(file = paste("Data/lassa_fever_analysis/", var, "_over_time.pdf", sep=""),   # The directory you want to save the file in
     #width = 7.4, # The width of the plot in inches
    # height = 5.85) # The height of the plot in inches
 
 ggplot(data = long_data, aes(x = day, y = val, group = ID, colour = outcome)) +
-  geom_line(aes(color = as.factor(outcome), linetype = as.factor(outcome)), size = 0.7) +
-  scale_linetype_manual(values = c("longdash", "solid")) + 
+  #geom_line(aes(color = as.factor(outcome), linetype = as.factor(outcome)), size = 0.7) +
+  #scale_linetype_manual(values = c("longdash", "solid")) + 
   scale_color_manual(values = c("black", "red")) + theme_bw() +
   theme(legend.position = "", panel.border = element_blank(), panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + 
   labs(x = "day", y = var, colour = "outcome ") +
-  xlim(0, 25) + 
+  xlim(0, 5) + 
   ggtitle(var) +
   theme(plot.title = element_text(hjust = 0.5)) +
-  ylim(0,100)
+  ylim(0,100) + 
+  geom_smooth(aes(group = outcome))
 
 #dev.off()
- 
+
+##### count values by day for patients who died
+out1 <- subset(long_data, outcome == 1)
+out1 %>%
+  group_by(day) %>%
+  count()
